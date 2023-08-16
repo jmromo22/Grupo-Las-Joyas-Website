b2ed6d36d408db092a91e8b6aecf45dff449bfda
jmromo22 <romojesus03@gmail.com>
2023-08-15 22:49:21 -0700

    Added NavBar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/NavBar.css';
+
+export default function NavBar() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<a href=''></a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
