d1d3eefe396a0656e4eac6f1a7e002208f157513
jmromo22 <romojesus03@gmail.com>
2023-08-21 22:11:09 -0700

    Added Hyperlinks

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './NavBar.module.css';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import instagramLogo from '../../assets/instagramLogo.png';
+import facebookLogo from '../../assets/facebookLogo.png';
+import yelpLogo from '../../assets/yelpLogo.png';
 
 export default function NavBar() {
 	return (
@@ -16,9 +19,15 @@ export default function NavBar() {
 				<Link to='/contact'>Contact</Link>
 			</div>
 			<div className={styles.socials}>
-				<Link to='https://www.instagram.com/lasjoyas14k/?hl=en'>Insta</Link>
-				<Link to='https://www.facebook.com/profile.php?id=100009084781103'>Facebook</Link>
-				<Link to='https://yelp.to/yXIJAC2Dpt'>Yelp</Link>
+				<Link to='https://www.instagram.com/lasjoyas14k/?hl=en'>
+					<img src={instagramLogo} alt='Instagram' />
+				</Link>
+				<Link to='https://www.facebook.com/profile.php?id=100009084781103'>
+					<img src={facebookLogo} alt='Facebook' />
+				</Link>
+				<Link to='https://yelp.to/yXIJAC2Dpt'>
+					<img src={yelpLogo} alt='Yelp' />
+				</Link>
 			</div>
 		</nav>
 	);
